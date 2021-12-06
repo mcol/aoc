dddd4f52315e09b64be093991f26ec400fdbfa62
Marco Colombo <m.colombo@ed.ac.uk>
2021-12-06 14:40:56 +0000

    Add solution for day 3.

---
+++ 2021/src/day03.rs
@@ -0,0 +1,82 @@
+use std::collections::HashSet;
+use std::fs;
+
+pub fn day03() {
+    let input = "data/input-03.txt";
+    let size = if input.contains("example") { 5 } else { 12 };
+
+    // read the contents of the file
+    let file = fs::read_to_string(input).unwrap();
+    let mut vecs = Vec::new();
+    for line in file.lines() {
+        let vals: Vec<u32> = line
+            .chars()
+            .collect::<Vec<char>>()
+            .into_iter()
+            .map(|z| z.to_digit(10).unwrap())
+            .collect();
+        vecs.push(vals);
+    }
+
+    let mut sums = vec![0; size];
+    for vals in &vecs {
+        for idx in 0..size {
+            sums[idx] += vals[idx];
+        }
+    }
+
+    let (mut gamma, mut epsilon) = (0, 0);
+    let half = vecs.len() as u32 / 2;
+    for idx in 0..size {
+        if sums[idx] > half {
+            gamma += i32::pow(2, (size - 1 - idx) as u32);
+        } else {
+            epsilon += i32::pow(2, (size - 1 - idx) as u32);
+        }
+    }
+    println!("Part 1: {}", gamma * epsilon);
+
+    let oxy = find_oxy_co2(&vecs, size, true);
+    let co2 = find_oxy_co2(&vecs, size, false);
+    println!("Part 2: {}", oxy * co2);
+}
+
+fn find_oxy_co2(vecs: &Vec<Vec<u32>>, size: usize, is_oxy: bool) -> i32 {
+    let mut keep = HashSet::new();
+    for idx in 0..vecs.len() {
+        keep.insert(idx);
+    }
+
+    for idx in 0..size {
+        let mut sum = 0;
+        for num in 0..vecs.len() {
+            if keep.contains(&num) {
+                sum += vecs[num][idx];
+            }
+        }
+        let val = if sum as f32 >= keep.len() as f32 / 2. {
+            is_oxy as u32
+        } else {
+            !is_oxy as u32
+        };
+
+        for (num, vals) in vecs.iter().enumerate() {
+            if keep.contains(&num) && vals[idx] != val {
+                keep.remove(&num);
+            }
+        }
+        if keep.len() == 1 {
+            break;
+        }
+    }
+
+    let idx: usize = *keep.iter().collect::<Vec<&usize>>()[0];
+    let val = &vecs[idx];
+    let mut res = 0;
+    for idx in 0..size {
+        if val[idx] == 1 {
+            res += i32::pow(2, (size - 1 - idx) as u32);
+        }
+    }
+    return res;
+}
